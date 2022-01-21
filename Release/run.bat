0607d5942b22751559e6727adf7861acde9de20e
OYAMIBE <90898887+OYAMIBE@users.noreply.github.com>
2022-01-21 18:25:19 +0800

    Add files via upload

---
+++ Release/run.bat
@@ -0,0 +1,19 @@
+ECHO OFF 
+
+REM Just run brokenithm-kb
+.\brokenithm-kb.exe
+
+REM View help
+REM .\brokenithm-kb.exe -h
+
+REM Run on a different port
+REM .\brokenithm-kb.exe -p 1117
+
+REM Run polling rate of 1000 times a second
+REM .\brokenithm-kb.exe -f 1000
+
+REM Run in verbose mode to check if button presses are detected
+REM .\brokenithm-kb.exe -v
+
+REM Run in verbose + dry run mode which doesn't send any keypreses
+REM .\brokenithm-kb.exe -v -d
